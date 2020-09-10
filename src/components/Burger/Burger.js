4e8d7e94b57d482aa48cd413f03172e298170e67
Matthew David <matthew.david@ruralsource.com>
2020-09-10 15:42:17 -0600

    add state to the builder and add ingredients dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,13 +4,24 @@ import classStyles from "./BurgerIngredient/Burger.module.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const Burger = (props) => {
+  let ingredientsList = Object.keys(props.ingredients)
+    .map((ingKey) => {
+      return [...Array(props.ingredients[ingKey])].map((_, i) => {
+        return <BurgerIngredient key={ingKey + i} type={ingKey} />;
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+
+  if (ingredientsList.length === 0) {
+    ingredientsList = <p>Please add some ingredients</p>;
+  }
+  console.log(ingredientsList);
   return (
     <div className={classStyles.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="lettuce" />
-      <BurgerIngredient type="bacon" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {ingredientsList}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,10 +4,19 @@ import Shell from "../../hoc/Shell";
 import Burger from "../../components/Burger/Burger";
 
 class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      lettuce: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0,
+    },
+  };
+
   render() {
     return (
       <Shell>
-        <Burger />
+        <Burger ingredients={this.state.ingredients} />
         <div>Build Controls</div>
       </Shell>
     );
